9f9bf31c84c5e25b9732bc28f622d4d1f6c8bcdd
James <jwu277@gmail.com>
2019-06-27 17:39:14 -0700

    Added some code in main.cpp to test compilation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
+// Arduino Modules
 #include <Arduino.h>
+
+// Device Modules
+#include <DriveMotor.hpp>
+
+// Define Devices
+DriveMotor left_drive_motor = DriveMotor(PA_0, PA_1, 1000, 100); // TODO: change
+
 
 void setup() {
   // put your setup code here, to run once:
+  left_drive_motor.init();
 }
 
 void loop() {
