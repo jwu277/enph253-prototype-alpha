fcd1c3d962163a7569589b7a2c6fc4eae2874abe
James <jwu277@gmail.com>
2019-06-28 11:41:34 -0700

    Added base structure for main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,78 @@
 #include <Arduino.h>
 
 // Device Modules
-#include <DriveMotor.hpp>
+#include "DriveMotor.hpp"
+
+// Constants
+#define PWM_CLK_FREQ 1000000
+#define PWM_PERIOD 100
+
+// Functions
+void actuator_init();
+void sensor_init();
+
+void read_sensors();
+void compute();
+void update_actuators();
+void run_actuators();
 
 // Define Devices
-DriveMotor left_drive_motor = DriveMotor(PA_0, PA_1, 1000, 100); // TODO: change
-
+DriveMotor left_drive_motor = DriveMotor(PA_0, PA_1, PWM_CLK_FREQ, PWM_PERIOD);
+DriveMotor right_drive_motor = DriveMotor(PA_2, PA_3, PWM_CLK_FREQ, PWM_PERIOD);
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  actuator_init();
+  sensor_init();
+
+}
+
+void actuator_init() {
+
   left_drive_motor.init();
+  right_drive_motor.init();
+
+}
+
+void sensor_init() {
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  // TODO: incorporate interrupts
+
+  // 1. Read new data from sensors
+  read_sensors();
+
+  // TODO: maybe merge compute() with update_actuators() ?
+
+  // 2. Perform computations
+  compute();
+
+  // 3. Update actuators in SW
+  update_actuators();
+
+  // 4. Tick the actuators in HW
+  run_actuators();
+
+}
+
+void read_sensors() {
+
+}
+
+void compute() {
+
+}
+
+void update_actuators() {
+
+}
+
+void run_actuators() {
+
+  left_drive_motor.actuate();
+  right_drive_motor.actuate();
+
 }
